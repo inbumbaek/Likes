d7e2c64c02eaf6f1513f1145de5d867790af52c2
inbumbaek <inbumbaek@gmail.com>
2023-03-15 12:02:47 -0700

    Click Likes

---
+++ likinglikes.js
@@ -0,0 +1,10 @@
+console.log("page loading...");
+
+var likeCount = 3;
+var likeSpan1 = document.querySelector("#post-1");
+
+
+function like() {
+    likeCount++;
+    likeSpan1.innerText = likeCount + "like(s)";
+}
